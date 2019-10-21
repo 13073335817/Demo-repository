9ba536572a54caea11c832db64c8798ed6932a17
root <1303726881@qq.com>
2019-10-21 15:20:43 +0800

    第一次上传

---
+++ src/main/java/com/itheima/Demon01.java
@@ -0,0 +1,7 @@
+package com.itheima;
+
+public class Demon01 {
+   public void run(){
+       System.out.println("============job executes===========");
+   }
+}
---
+++ src/main/java/com/itheima/Demon02.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Demon02 {
+    public static void main(String[] args) {
+        new ClassPathXmlApplicationContext("application-jobs.xml");
+    }
+}
